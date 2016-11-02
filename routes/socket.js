a20d9aaf0bfed999cd039ebd40ba44775ef88254
Quang Le <me@quangld.com>
2016-11-02 11:10:22 +0700

    added node-express, moved static files to public

---
+++ routes/socket.js
@@ -0,0 +1,11 @@
+function Socket (server) {
+  var io = require('socket.io')(server)
+  io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' })
+    socket.on('my other event', function (data) {
+      console.log(data)
+    })
+  })
+}
+
+module.exports = Socket
